0630603795562b62fe2734928a741cf18f8e3187
Travis Gersch <t_gersch@hotmail.com>
2022-07-12 11:34:31 -0500

    arrays and list exercise initial commit

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,13 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
-
+            var digits = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            var notOdd = new List<int>();
+            var notEven = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +24,48 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            //foreach(var thingy in digits) ////for each loop
+            //{
+            //    if(thingy % 2 == 0)
+            //    {
+            //        notOdd.Add(thingy);
+            //    }
+            //    else
+            //    {
+            //        notEven.Add(thingy);
+            //    }
+            //}
 
+            for(int i = 0; i < digits.Length; i++) ////for loop
+            {
+                if (digits[i] % 2 == 0)
+                {
+                    notOdd.Add(digits[i]);
+                }
+                else
+                {
+                    notEven.Add(digits[i]);
+                }
+            }
+            
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            Console.WriteLine("Evens");
+            foreach(var thingy in notOdd)
+            {
+                Console.WriteLine(thingy);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Odds");
+            foreach(var thingy in notEven)
+            {
+                Console.WriteLine(thingy);
+            }
         }
     }
 }
